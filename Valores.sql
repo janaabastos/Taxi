f666d7002fcc0455668879a713655fddd513c1b1
janaabastos <janainabs10@gmail.com>
2016-10-03 00:00:45 -0400

    Insert into

---
+++ Valores.sql
@@ -0,0 +1,34 @@
+insert into cliente
+(nome,cpf,endereco,telefone)
+values
+('Joao','02117459285','rua do limoeiro 98','997873478');
+
+insert into corrida
+(quantidade,km_rodados,valor_por_corrida)
+values
+('4','7890','4,00');
+
+insert into empresa
+(nome,cnpj,telefone)
+values
+('Joao','093580923802934','998765467');
+
+insert into motorista
+(nome,cpf,Cart_hab,endereco,telefone)
+values
+('joao','02187837498','9283709247','vila da prata','30227546');
+
+insert into ocorrencia
+(data_hora, tipo,local)
+values
+('19/10 4h','assalto','centro');
+
+insert into resgistroChamada
+(data,hora,percurso,caracteristicas)
+values
+('15/10','5h','do centro para a zona leste','veiculo em alta velocidade');
+
+insert into veiculos
+(placa,modelo,cor)
+values
+('OAH-3456','siena','prata');
